01d2f482c47cddf5834e3117c764baa59469a07f
Shopik <sh_sergey@tut.by>
2016-10-05 16:52:03 +0300

    first steps in UI

---
+++ app/src/main/java/com/example/shopik/vk/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.shopik.vk;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Shader;
+import android.graphics.drawable.BitmapDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Layout;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}

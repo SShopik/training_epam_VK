5a8035046689c8046ad2346467a46ab88a2a1dd0
Shopik <sh_sergey@tut.by>
2016-10-06 15:44:42 +0300

    add round foto

--- app/src/main/java/com/example/shopik/vk/MainActivity.java
+++ app/src/main/java/com/example/shopik/vk/MainActivity.java
@@ -1,30 +1,40 @@
 package com.example.shopik.vk;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Shader;
-import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Layout;
-import android.view.Gravity;
 import android.view.View;
-import android.widget.LinearLayout;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Bitmap bitmap;
+    Bitmap finalBitmap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.image_3);
+        finalBitmap = BitmapRound.getCircleMaskedBitmapUsingClip(bitmap,45);
+        ImageView imageView = (ImageView) findViewById(R.id.ava);
+        imageView.setImageBitmap(finalBitmap);
 
+        bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.image_2);
+        finalBitmap = BitmapRound.getCircleMaskedBitmapUsingClip(bitmap,45);
+        imageView = (ImageView) findViewById(R.id.ava_2);
+        imageView.setImageBitmap(finalBitmap);
     }
-    static final String NUU = "NUUUU";
-    public void onClick(View view){
 
-        Toast toast = Toast.makeText(getApplicationContext(),NUU,Toast.LENGTH_SHORT);
+    static final String CLICK = "Click";
+    public void onClick(View view){
+        Toast toast = Toast.makeText(getApplicationContext(),CLICK,Toast.LENGTH_SHORT);
         toast.show();
+    }
+    public void startMenu(View view){
+        startActivity(new Intent(this, Menu.class));
     }
 }
---
+++ app/src/main/java/com/example/shopik/vk/Menu.java
@@ -0,0 +1,29 @@
+package com.example.shopik.vk;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class Menu extends AppCompatActivity {
+
+    Bitmap bitmap;
+    Bitmap finalBitmap;
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu);
+
+        bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.image_3);
+        finalBitmap = BitmapRound.getCircleMaskedBitmapUsingClip(bitmap,50);
+
+
+        ImageView imageView = (ImageView) findViewById(R.id.image_round);
+        imageView.setImageBitmap(finalBitmap);
+
+    }
+
+
+}

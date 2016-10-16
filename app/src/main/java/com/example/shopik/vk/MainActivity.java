aa362532aea856f9a92b5df122d986034c9654cb
Shopik <sh_sergey@tut.by>
2016-10-16 19:50:38 +0300

    add flavor

--- app/src/main/java/com/example/shopik/vk/MainActivity.java
+++ app/src/main/java/com/example/shopik/vk/MainActivity.java
@@ -3,12 +3,12 @@ package com.example.shopik.vk;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
-
 public class MainActivity extends AppCompatActivity {
 
     Bitmap bitmap;
@@ -18,6 +18,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView = (TextView) findViewById(R.id.action_bar_news);
+        String s = BuildConfig.NAME;
+        textView.setText(String.valueOf("Новости "+s));
 
         bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.image_3);
         finalBitmap = BitmapRound.getCircleMaskedBitmapUsingClip(bitmap, 45);
